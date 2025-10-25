e52c842e42c33627b2b783387a153e7603e41f76
shahadot786 <shrhossain786@gmail.com>
2025-10-25 21:51:51 +0600

    change project name

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
+import React from "react";
 
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex justify-center items-start pt-20">
-      <div className="w-[400px] max-w-md bg-white shadow-lg rounded-lg p-6">
-        <h1 className="text-2xl font-bold mb-4 text-center">📝 Todo App</h1>
-        <TodoForm />
-        <TodoList />
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
