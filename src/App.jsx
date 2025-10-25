4590faa2f4b5111287077ed95f2b5da4cc95e410
shahadot786 <shrhossain786@gmail.com>
2025-10-25 23:46:58 +0600

    add all assets

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+      <h1 class="text-3xl font-bold underline text-red-500">Hello world!</h1>
     </div>
   );
 };
